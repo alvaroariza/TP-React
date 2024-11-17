6ead77e3cb77216ef7503a9727e854d69dfa6cdf
alvaroariza <alvaroariza03@gmail.com>
2024-11-17 15:16:43 -0300

    primer commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+import { ComponentCounter } from "./components/ComponentCounter/ComponentCounter"
+import { MiPrimerComponent } from "./components/MiPrimerComponent"
+import { ComponentUseEffect } from "./components/ComponentUseEffect/ComponentUseEffect"
+import { FormComponent } from "./components/FormComponent/FormComponent"
+import { AppProduct } from "./components/AppProduct/AppProduct"
+
+export const App = () => {
+  return (
+    <div style={{display:'flex', flexDirection:'column', gap:'2vh'}}>
+      {/* <MiPrimerComponent text={"Texto desde propiedades"} color="red" fontSize={5}/>
+      <ComponentCounter/>
+      <ComponentUseEffect/> */}
+      {/* <FormComponent/> */}
+      <AppProduct/>
+    </div>
+  )
+}
